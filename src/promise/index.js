58cfe327d9813a6416672429fe6dc5bf3786ff86
vabklb <v.barilin@itecriocuarto.org.ar>
2021-12-27 21:36:17 -0300

    add files

--- src/promise/index.js
+++ src/promise/index.js
@@ -1,6 +1,6 @@
 const somethingWillHappen = () => {
     return new Promise((resolve, reject) => {
-        if (false) {
+        if (true) {
             resolve('Hey!');
         } else {
             reject('Upss');
@@ -11,3 +11,23 @@ const somethingWillHappen = () => {
 somethingWillHappen()
     .then(response => console.log(response))
     .catch(err => console.log(err));
+
+const somethingWillHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            setTimeout(() => {resolve('funcionaaaa en 2000ms')},2000)
+        } else {
+            const error = new Error('Whooops');
+            reject(error);
+        }
+    })
+}
+somethingWillHappen2()
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+    .then(response => {
+        console.log('array of results', response);
+    })
+    .catch(error => console.error(error));
